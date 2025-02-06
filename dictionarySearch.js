71d652df202a21de0107b672a774a80542fa1b6f
Chapman <chapman.chappelle@gmail.com>
2025-02-06 15:57:58 -0500

    finished functionality

--- dictionarySearch.js
+++ dictionarySearch.js
@@ -1,18 +1,26 @@
-//API CALLS
+// API CALLS
 const API = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.local.get('word', async (data) => {
-    console.log('highlighted ' + data.word);
+    console.log('Highlighted word: ' + data.word);
     const response = await fetch(API + data.word);
     const result = await response.json();
+
     let definitionText = 'Highlight a word to get its definition!';
-    let word = '';
-    if (Array.isArray(result) && result[0].meanings.length > 0) {
-      definitionText = result[0].meanings[0].definitions[0].definition;
-      //word = FILL IN THIS
+
+    if (Array.isArray(result) && result.length > 0) {
+      definitionText = '';
+
+      result[0].meanings.forEach((meaning) => {
+        definitionText += `(${meaning.partOfSpeech})\n`;
+        meaning.definitions.forEach((def, index) => {
+          definitionText += `${index + 1}. ${def.definition}\n`;
+        });
+        definitionText += '\n';
+      });
     }
-    document.getElementById('word').textContent = word; //ADD WORD TO THIS
+
     document.getElementById('definition').textContent = definitionText;
   });
 });
