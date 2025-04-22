54949044c1edec24f7c681a18d3a4c09437ad422
MoniqueNunez <mnunezchiro@gmail.com>
2025-04-22 11:22:10 -0500

    deleted duplicates

--- README.md
+++
@@ -1,3 +0,0 @@
-# DictionaryExtension
-
-Hackathon
--- content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mouseup', async () => {
-  let selectedWord = window.getSelection().toString().trim();
-  if (selectedWord.length > 0) {
-    chrome.storage.local.set({ word: selectedWord });
-  }
-});
